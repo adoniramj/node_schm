691fe1136905b50a3e53daa5b01526b7d98965d6
adoniramj <adoniramjvargas@gmail.com>
2020-01-04 14:10:31 -0500

    prettier

--- node-farm/index.js
+++ node-farm/index.js
@@ -36,40 +36,39 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 
 const dataObj = JSON.parse(data)
-const slug = dataObj.map(el => slugify(el.productName, {lower: true}))
+const slug = dataObj.map(el => slugify(el.productName, { lower: true }))
 
 console.log(slug)
-const server = http.createServer((req,res) => {
+const server = http.createServer((req, res) => {
   const pathName = req.url
   const { query, pathname } = url.parse(req.url, true)
   //overview
-  if(pathname === '/' || pathname === '/overview') {
-    res.writeHead(200, { 'Content-type' : 'text/html' })
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' })
     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
     res.end(output)
-  } 
-  
+  }
   //product page
   else if (pathname === '/product') {
-      //console.log(query)
-      res.writeHead(200, { 'Content-type' : 'text/html' })
-      const product = dataObj[query.id]
-      const output = replaceTemplate(tempProduct, product)
-      res.end(output)
+    //console.log(query)
+    res.writeHead(200, { 'Content-type': 'text/html' })
+    const product = dataObj[query.id]
+    const output = replaceTemplate(tempProduct, product)
+    res.end(output)
   } else if (pathname === '/api') {
-      res.writeHead(200, { 'Content-type' : 'application/json' })
-      res.end(data)
+    res.writeHead(200, { 'Content-type': 'application/json' })
+    res.end(data)
   }
   else {
     res.writeHead(404, {
-      'Content-type' : 'text/html',
-      'my-own-header' : 'hello-world'
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
     })
     res.end('<h1>Page not found!</h1>')
   }
 })
 
-server.listen(8000,'127.0.0.1', () => {
+server.listen(8000, '127.0.0.1', () => {
   console.log('Server is listening on port 8000')
 })
